c46968324c29e7fcb18c31a9feed9713476eb1d6
ssyssoev <ssyssoev@mail.ru>
2023-06-05 09:01:13 +0300

    Сделано ДЗ№2

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main{
         task6();
         task7();
         task8();
+        System.out.println(" Домашнее задание №2 ");
         }
      public static void task1() {
         var dog=8.0;
